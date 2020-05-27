d551eee86a5a4084bfd554587a7a02bf1fe977fd
Diego Tobalina <diegotobalina97@gmail.com>
2020-05-27 13:38:17 +0200

    username nullable login

--- src/main/java/com/example/demo/model/dto/LoginInputDto.java
+++ src/main/java/com/example/demo/model/dto/LoginInputDto.java
@@ -2,6 +2,7 @@ package com.example.demo.model.dto;
 
 import com.example.demo.model.validator.PasswordConstraint;
 import com.example.demo.model.validator.UsernameConstraint;
+import com.example.demo.model.validator.UsernameNullableConstraint;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,7 +19,7 @@ import javax.validation.constraints.Null;
  * @author diegotobalina
  */
 @AllArgsConstructor @Getter @Setter @ToString public class LoginInputDto {
-    @ApiModelProperty(example = "user") @UsernameConstraint private String username;
+    @ApiModelProperty(example = "user") @UsernameNullableConstraint private String username;
     @ApiModelProperty(example = "user@user.com") @Email private String email;
     @ApiModelProperty(example = "password") @PasswordConstraint private String password;
 }
