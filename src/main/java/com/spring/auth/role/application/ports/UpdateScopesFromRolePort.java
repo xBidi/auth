9f3a3c8918398e0010bb2c289aec4c9141d2db86
xBidi <diegotobalina97@gmail.com>
2020-10-19 15:36:39 +0200

    fix packages

--- src/main/java/com/spring/auth/role/application/ports/UpdateScopesFromRolePort.java
+++ src/main/java/com/spring/auth/role/application/ports/UpdateScopesFromRolePort.java
@@ -1,13 +1,14 @@
-package com.bosonit.staffit.auth.role.application.ports;
+package com.spring.auth.role.application.ports;
 
-import com.bosonit.staffit.auth.exceptions.application.DuplicatedKeyException;
-import com.bosonit.staffit.auth.exceptions.application.NotFoundException;
-import com.bosonit.staffit.auth.role.domain.Role;
+
+import com.spring.auth.exceptions.application.DuplicatedKeyException;
+import com.spring.auth.exceptions.application.NotFoundException;
+import com.spring.auth.role.domain.Role;
 
 import java.util.List;
 
 /** @author diegotobalina created on 24/06/2020 */
 public interface UpdateScopesFromRolePort {
   Role update(Role role, List<String> scopeIds) throws DuplicatedKeyException;
-  Role update(String roleId, List<String> scopeIds) throws NotFoundException, DuplicatedKeyException;
+  Role update(String roleId, List<String> scopeIds) throws NotFoundException, DuplicatedKeyException, NotFoundException;
 }
